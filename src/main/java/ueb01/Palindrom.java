e0ef02eaed7adaf689feb71713c8055a1620ea63
sWIFnaoese <nadine.oeser@stud.fh-rosenheim.de>
2020-01-11 01:04:51 +0100

    Palindrom gelöst

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -1,9 +1,32 @@
 package ueb01;
 
-class Palindrom {
+public class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
+		if (s.length() == 1) {
+			return true;
+		}
+		else{
+			CharStack stack = new CharStackImpl();
+			char[] cs = s.toLowerCase().replaceAll(" ", "").toCharArray();
+			// die erste Hälfte auf den Stack legen
+			int i = 0;
+			for (; i < cs.length / 2; i++) {
+				stack.push(cs[i]);
+			}
 
-		return false;
+			// ungerade Länge: ignoriere Buchstaben in der Mitte
+			if (cs.length % 2 == 1) {
+				i++;
+			}
+
+			for (; i < cs.length; i++) {
+				if (stack.pop() != cs[i])
+					return false;
+			}
+
+
+		}
+		return true;
 	}
 }
